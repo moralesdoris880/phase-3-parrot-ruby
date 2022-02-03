068e0fc550d214a2f45a65a33287f78f35c40865
Doris Morales <moralesdoris880@gmail.com>
2022-02-03 12:24:01 -0500

    Completed Assignment

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+#The parrot method should accept an argument of a string and both puts out that string and return the string at the end of the method.
+
+#The parrot method should have a default argument of "Squawk!".
+
+#Note: This lab is explicitly testing your ability to control the return value of a method, not just what it does, but what it returns. Remember, return values are important. Ask yourself? what is the return value of puts?
+
+
+def parrot(phrase="Squawk!")
+    puts "#{phrase}"
+    phrase
+end
